8b62f44b328d3cc3f73593edcf9236f7b6564269
dnrtk <dnrtk@yahoo.co.jp>
2022-12-18 15:07:19 +0900

    debug ok

--- main.c
+++ main.c
@@ -4,5 +4,16 @@
 
 void main(void) 
 {
-    printf("hw\n");
+    char *s = malloc(1024);
+    snprintf(s, 1024, "asdfghjk_key_qwert_val_zxcv\nmnbvcxz_val_mnb-key\n");
+    printf("%s\n", s);
+
+    char *ss = str_replace(s,"key", "KKEEYY");
+    printf("%s\n", ss);
+
+    // char *sss = str_replace(ss,"val", "VL");
+    char *sss = str_replace(ss,"val", "あいう");
+    printf("%s\n", sss);
+
+    free(sss);
 }
--- strutil.c
+++ strutil.c
@@ -11,10 +11,11 @@ char* str_replace(char *org, char *src, char *dst) {
     size_t new_len = org_len;
     char *org_pos = org;
     while ((org_pos = strstr(org_pos, src)) != NULL) {
-        org_len += dst_len - src_len;
+        new_len += dst_len - src_len;
+        org_pos += src_len;
     }
     // 置換後の文字列長で新領域を確保
-    char *new = realloc(org, new_len);
+    char *new = malloc(new_len);
     if (NULL == new) {
         printf("[%s] realloc faied. new_len=%ld\n", __func__, new_len);
         free(org);
@@ -26,6 +27,7 @@ char* str_replace(char *org, char *src, char *dst) {
         *org_pos = '\0';
         strcat(new, org_base);
         strcat(new, dst);
+        org_base = org_pos + src_len;
     }
     strcat(new, org_base);
     free(org);
